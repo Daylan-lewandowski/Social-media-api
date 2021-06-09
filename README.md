fbda407ac03b2bd93336fea78dbf60865e40c513
Daylan-lewandowski <74326816+Daylan-lewandowski@users.noreply.github.com>
2021-06-09 10:46:20 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Social Media API - Challenge 18
+
+## Description
+A REST API for a social media app. Built with Express, Mongoose, and MongoDB
+
+## Tech used
+- express
+- moment
+- mongoose
+
+## How to use
+1. Make sure you have MongoDB installed on your machine (if you don't, follow the instructions on the MongoDB Website
+2. Clone the repo
+3. Install dependencies with `npm -i`
+4. Run `npm start` to run the server and make the API live
+5. Use your browser to test the REST API.
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/challenge-18', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -17,4 +17,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-ap
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
